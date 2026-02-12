2cb972d376179c600fe8b5aebff08362be03719d
João Henrique <jhenrique.caval@hotmail.com>
2026-02-12 13:35:46 -0300

    Criação de Login e Cadastro + estilizações em outros arquivos

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Footer from "./components/footer/Footer";
 import Navbar from "./components/navbar/Navbar";
+import Cadastro from "./pages/cadastro/Cadastro";
 import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
 
 function App() {
   return (
     <>
-      <Navbar />
-      <Home />
-      <Footer />
+      <BrowserRouter>
+        <Navbar />
+        <div className="min-h-[80vh]">
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/cadastro" element={<Cadastro />} />
+            <Route path="/home" element={<Home />} />
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
     </>
   );
 }
--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -16,9 +16,18 @@ function Footer() {
           </p>
           <p className="text-lg">Acesse nossas redes sociais</p>
           <div className="flex gap-2">
-            <LinkedinLogoIcon size={48} weight="bold" />
-            <InstagramLogoIcon size={48} weight="bold" />
-            <FacebookLogoIcon size={48} weight="bold" />
+            <a href="https://www.linkedin.com/in/joao-hrnk" target="_blank">
+              <LinkedinLogoIcon size={48} weight="bold" />
+            </a>
+            <a href="https://www.instagram.com/hnrk_cav/" target="_blank">
+              <InstagramLogoIcon size={48} weight="bold" />
+            </a>
+            <a
+              href="https://www.facebook.com/profile.php?id=100001909494915"
+              target="_blank"
+            >
+              <FacebookLogoIcon size={48} weight="bold" />
+            </a>
           </div>
         </div>
       </div>
