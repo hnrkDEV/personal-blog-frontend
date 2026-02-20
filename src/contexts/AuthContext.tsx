e24733d331a7e7106cea546b22fc54bc3f59d2a2
João Henrique <jhenrique.caval@hotmail.com>
2026-02-19 23:28:25 -0300

    Módulo de login realizado

---
+++ src/contexts/AuthContext.tsx
@@ -0,0 +1,62 @@
+import { createContext, useState, type ReactNode } from "react";
+import type UsuarioLogin from "../models/UsuarioLogin";
+import { login } from "../services/Service";
+
+interface AuthContextProps {
+  usuario: UsuarioLogin;
+  handleLogout(): void;
+  handleLogin(usuario: UsuarioLogin): Promise<void>;
+  isLoading: boolean;
+}
+
+interface AuthProviderProps {
+  children: ReactNode;
+}
+
+export const AuthContext = createContext({} as AuthContextProps);
+
+export function AuthProvider({ children }: AuthProviderProps) {
+  const [usuario, setUsuario] = useState<UsuarioLogin>({
+    id: 0,
+    name: "",
+    username: "",
+    picture: "",
+    password: "",
+    token: "",
+  });
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  async function handleLogin(usuarioLogin: UsuarioLogin) {
+    setIsLoading(true);
+
+    try {
+      await login("/usuarios/login", usuarioLogin, setUsuario);
+      alert("Usuário logado com sucesso!");
+    } catch (error) {
+      console.log(error);
+      alert("Erro ao fazer login!");
+    }
+
+    setIsLoading(false);
+  }
+
+  function handleLogout() {
+    setUsuario({
+      id: 0,
+      name: "",
+      username: "",
+      picture: "",
+      password: "",
+      token: "",
+    });
+  }
+
+  return (
+    <AuthContext.Provider
+      value={{ usuario, handleLogin, handleLogout, isLoading }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+}
